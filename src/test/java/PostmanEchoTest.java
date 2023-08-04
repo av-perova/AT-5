d8d1fbbb332523c404739c24451543c5a81b9fba
Perova <av_perova@mail.ru>
2023-08-04 12:34:25 +0700

    test success

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,6 +17,6 @@ public class PostmanEchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("tes"));
+                .body("data", equalTo("test"));
     }
 }
